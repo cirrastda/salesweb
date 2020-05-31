ec4f1d2c6a4b54ef003a4a963217fdb885bb1afc
Anderson Matuchenko <anderson.matuchenko@buonny.com.br>
2020-05-31 10:56:24 -0300

    Seller Service and Controller Index

---
+++ SalesWeb/Controllers/SellersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Models;
+using SalesWeb.Services;
+
+namespace SalesWeb.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Sellers";
+            List<Seller> sellers = _sellerService.FindAll();
+            return View(sellers);
+        }
+    }
+}
+ 
