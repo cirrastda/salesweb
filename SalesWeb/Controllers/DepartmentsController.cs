338722f5addbd85312cf485909c6aa7d75a96e5f
Anderson Matuchenko <anderson.matuchenko@buonny.com.br>
2020-05-31 07:45:47 -0300

    Model / Controller / View Departments

---
+++ SalesWeb/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Models;
+
+namespace SalesWeb.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+
+            departments.Add(new Department { Id = 1, Name = "Electronics" });
+            departments.Add(new Department { Id = 2, Name = "Computer" });
+            return View(departments);
+        }
+    }
+}
