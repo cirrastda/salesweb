de46a27bc2927c0cc067596e08f9d59efba63ab5
Anderson Matuchenko <anderson.matuchenko@buonny.com.br>
2020-06-06 11:08:59 -0300

    Simple Search

---
+++ SalesWeb/Controllers/SalesRecordsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Services;
+
+namespace SalesWeb.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "Sales Records";
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? start, DateTime? end)
+        {
+            ViewData["Title"] = "Sales Records - Simple Search";
+            if (!start.HasValue) { start = new DateTime(DateTime.Now.Year, 1, 1); }
+            if (!end.HasValue) { end = DateTime.Now; }
+            ViewData["start"] = start.Value.ToString("dd/MM/yyyy");
+            ViewData["end"] = end.Value.ToString("dd/MM/yyyy");
+            
+            var salesRecords = await _salesRecordService.FindByDateAsync(start, end);
+            return View(salesRecords);
+        }
+
+        public async Task<IActionResult> GroupSearch()
+        {
+            ViewData["Title"] = "Sales Records - Group Search";
+            return View();
+        }
+    }
+}
---
+++ SalesWeb/Services/SalesRecordService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWeb.Data;
+using SalesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWeb.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebContext _context;
+
+        public SalesRecordService(SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? start, DateTime? end)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (start.HasValue) {
+                result = result.Where(item => item.Date >= start.Value);
+            }
+            if (end.HasValue)
+            {
+                result = result.Where(item => item.Date >= start.Value);
+            }
+            result = result.Include(item => item.Seller)
+                            .Include(item => item.Seller.Department)
+                            .OrderByDescending(item => item.Date);
+
+            return await result.ToListAsync();
+            /*
+            return await _context.SalesRecord
+                .Where(item => item.Date >= start.GetValueOrDefault() && item.Date <= end)
+                .Include(item => item.Seller)
+                .Include(item => item.Seller.Department)
+                .OrderBy(item => item.Date)
+                .ToListAsync();
+            */
+        }
+    }
+}
